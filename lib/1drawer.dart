2cace0db7bd9518bd42508264a3f82bc27ffaa9d
tihitna211 <tihitnasol1@gmail.com>
2024-11-01 13:28:54 +0300

    add comment

---
+++ lib/1drawer.dart
@@ -0,0 +1,291 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:newproj/Languages.dart';
+import 'package:newproj/Slider.dart';
+import 'package:newproj/editprofilepage.dart';
+import 'package:newproj/main.dart';
+import 'package:newproj/searchpage.dart';
+import 'package:newproj/spotit.dart';
+//import 'package:newproj/main.dart';
+
+class Drower extends StatefulWidget {
+  Drower({super.key});
+
+  @override
+  State<Drower> createState() => _DrowerState();
+}
+
+class _DrowerState extends State<Drower> {
+  final url =
+      "https://media.istockphoto.com/id/1460122390/photo/portrait-beauty-and-facial-with-a-black-woman-in-studio-to-promote-natural-skincare-or.jpg?s=1024x1024&w=is&k=20&c=OM9PF35xRZavW_xaS4Ru-3DVdJ_9HIy-VQHZMOLVBrk=";
+
+  bool Islight = false;
+  String name = "tihitna";
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    SliderPage(),
+    SeaechPage(),
+    LoginPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => _widgetOptions[index]),
+    );
+  }
+
+  List<String> imagePaths = [
+    'https://media.istockphoto.com/id/1460007178/photo/library-books-on-table-and-background-for-studying-learning-and-research-in-education-school.webp?s=1024x1024&w=is&k=20&c=cuzIXmvKHLpoGxGIft9zCiTw-jeL0Gjp7UNZau0MNkk=',
+    'https://media.istockphoto.com/id/803039496/photo/books-on-table-against-shelf-in-library-at-school.jpg?s=1024x1024&w=is&k=20&c=EG_B1AoCZL_lAfv4MCWr6LBRkL5M9-_taKGwGbKIDC4=',
+    'https://media.istockphoto.com/id/1419410282/photo/silent-forest-in-spring-with-beautiful-bright-sun-rays.jpg?s=1024x1024&w=is&k=20&c=K8yBJVB-TtpPF1O2zOhVlzXECDxJsadlRrLf4gXXNkk='
+  ];
+  List<String> title = [
+    'short',
+    'lily',
+    'roses',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme:
+              (Islight == true) ? ColorScheme.dark() : ColorScheme.light()),
+      debugShowCheckedModeBanner: false,
+      home: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'welcome $name!',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 41, 42, 43)),
+            ).tr(),
+            centerTitle: true,
+          ),
+          drawer: Drawer(
+            backgroundColor: (Islight == false)
+                ? Color.fromARGB(255, 238, 155, 155)
+                : Colors.grey[600],
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  UserAccountsDrawerHeader(
+                      decoration: BoxDecoration(
+                          color: (Islight == false)
+                              ? Color.fromARGB(255, 238, 155, 155)
+                              : Colors.grey[600],
+                          backgroundBlendMode: BlendMode.color),
+                      arrowColor: Colors.blueGrey,
+                      accountEmail: Text("tihitnasol21@gmail.com"),
+                      accountName: Text(name),
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage(url),
+                      )),
+                  ExpansionTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('setting').tr(),
+                    children: [
+                      ListTile(
+                          leading: const Icon(Icons.language),
+                          title: Text('changelanguage').tr(),
+                          trailing: Icon(Icons.check),
+                          onTap: () {
+                            Navigator.push(
+                                (context),
+                                MaterialPageRoute(
+                                  builder: (context) => const LanguageChanger(),
+                                ));
+                          }),
+                      SwitchListTile(
+                        value: Islight,
+                        onChanged: (value) {
+                          setState(() {
+                            Islight = !Islight;
+                          });
+                        },
+                        secondary: const Icon(Icons.format_paint),
+                        title: Text('change theme'),
+                      ),
+                    ],
+                  ),
+                  ListTile(
+                      leading: Icon(Icons.person_2),
+                      title: Text('profile').tr(),
+                      trailing: Icon(Icons.check),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => profileEdit(),
+                            ));
+                      }),
+                  ListTile(
+                      leading: Icon(Icons.logout_outlined),
+                      title: Text('logout').tr(),
+                      trailing: Icon(Icons.check),
+                      onTap: () {}),
+                ],
+              ),
+            ),
+          ),
+          body: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                    hintText: 'search', prefixIcon: Icon(Icons.search)),
+              ),
+              Text("Today is good day to learn something new thing!",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("trend courses",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  TextButton(
+                    child: Text('see all...'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              Flexible(
+                flex: 1,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Flexible(
+                        child: Container(
+                          child: Column(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Card(
+                                child: ListTile(
+                                    //tileColor: Colors.blueGrey,
+                                    leading: Icon(
+                                      Icons.book_online,
+                                      color: Colors.blue[900],
+                                    ),
+                                    title: Text(
+                                      'reading',
+                                    ),
+                                    subtitle: Text(
+                                      'u full person',
+                                    )),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Card(
+                                child: ListTile(
+                                    // tileColor: Colors.blueGrey,
+                                    leading: Icon(
+                                      Icons.book_online,
+                                      color: Colors.blue[900],
+                                    ),
+                                    title: Text('writing'),
+                                    subtitle: Text(
+                                      ' full person',
+                                    )),
+                              ),
+                            )
+                          ]),
+                          width: 220.0,
+                          height: 200,
+                          margin: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("suggested for u",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  TextButton(
+                    child: Text('see all...'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              Flexible(
+                flex: 1,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                          onDoubleTap: () =>
+                              Navigator.push((context), MaterialPageRoute(
+                                builder: (context) {
+                                  return Home(
+                                      // name: name[index % name.length].toString(),
+                                      // image:
+                                      //     imagePaths[index % imagePaths.length],
+                                      );
+                                },
+                              )),
+                          child: Container(
+                              width: 200.0,
+                              height: 150,
+                              margin: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Column(children: [
+                                Image.network(
+                                  imagePaths[index % imagePaths.length],
+                                  fit: BoxFit.cover,
+                                  height: 120,
+                                ),
+                                Text(title[index % title.length].toString()),
+                              ])));
+                    }),
+              ),
+              BottomNavigationBar(
+                currentIndex: _selectedIndex,
+                onTap: _onItemTapped,
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'Search',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'login',
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
---
+++ lib/spotit.dart
@@ -0,0 +1,329 @@
+// import 'dart:convert';
+// import 'dart:math';
+// import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 500) {
+        return const ForMobile();
+      } else {
+        return const ForTablet();
+      }
+    });
+  }
+}
+
+class ForMobile extends StatefulWidget {
+  const ForMobile({
+    super.key,
+  });
+
+  @override
+  State<ForMobile> createState() => _ForMobileState();
+}
+
+class _ForMobileState extends State<ForMobile> {
+// print(await http.read(
+//   Uri.https('example.com', 'foobar.txt')));
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  bool VisibleText = true;
+
+  var error = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Builder(builder: (context) {
+          return SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    Image.asset('assets/Asset 4.png',
+                        width: MediaQuery.of(context).size.width * 0.55),
+                    const SizedBox(height: 32),
+                    TextButton(
+                      onPressed: () {
+                        ;
+                        // Navigator.push(context, MaterialPageRoute(
+                        //   builder: (context) {
+                        //     return NextPage();
+                        //   },
+                        // ));
+                      },
+                      child: const Text(
+                        'sign in',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      child: TextFormField(
+                        controller: email,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.deepOrange,
+                          hintText: 'email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 19),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20)),
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      child: Expanded(
+                        child: TextField(
+                          controller: password,
+                          obscureText: VisibleText,
+                          onChanged: (value) {
+                            if (value.length < 8) {
+                              setState(() {
+                                error = "too short";
+                              });
+                            } else if (value.length >= 8 && value.length < 12) {
+                              setState(() {
+                                error = "medium";
+                              });
+                            }
+                            // ignore: curly_braces_in_flow_control_structures
+                            else {
+                              setState(() {
+                                error = "good!";
+                              });
+                            }
+                            ;
+                          },
+                          decoration: InputDecoration(
+                            errorText: error,
+                            errorStyle: TextStyle(
+                                color: (error == 'good!')
+                                    ? Colors.green
+                                    : (error == "too short")
+                                        ? Colors.red
+                                        : (error == 'medium')
+                                            ? Colors.amber
+                                            : null),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  VisibleText = !VisibleText;
+                                });
+                              },
+                              icon: Icon((VisibleText)
+                                  ? Icons.visibility
+                                  : (!VisibleText)
+                                      ? Icons.visibility_off_outlined
+                                      : null),
+                            ),
+                            hintText: "password",
+                            filled: true,
+                            fillColor: Colors.deepOrange,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                        child: GestureDetector(
+                      onDoubleTap: () {},
+                      child: Container(
+                          width: MediaQuery.of(context).size.width * 1,
+                          decoration: BoxDecoration(
+                              color: Color(0xffff3b00),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: const Image(
+                            image: AssetImage(
+                              'assets/Intersect.png',
+                            ),
+                            fit: BoxFit.cover,
+                          )
+                          // AssetImage('assets/intersect.png'),
+                          ),
+                    ))
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class ForTablet extends StatefulWidget {
+  const ForTablet({
+    super.key,
+  });
+
+  @override
+  State<ForTablet> createState() => _ForTabletState();
+}
+
+class _ForTabletState extends State<ForTablet> {
+  bool VisibleText = true;
+  var error = "";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Builder(builder: (context) {
+        return Scaffold(
+          backgroundColor: Colors.grey,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 32),
+                      TextButton(
+                        onPressed: () {
+                          // ;
+                        },
+                        child: const Text(
+                          'sign in',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        //decoration: BoxDecoration(color: Colors.orange),
+                        padding: const EdgeInsets.all(8.0),
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.deepOrange,
+                            hintText: 'email',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 9),
+                      Container(
+                        //color: Colors.orange,
+                        padding: const EdgeInsets.all(8.0),
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: TextFormField(
+                          obscureText: VisibleText,
+                          onChanged: (value) {
+                            if (value.length < 8) {
+                              setState(() {
+                                error = "too short";
+                              });
+                            } else if (value.length >= 8 && value.length < 12) {
+                              setState(() {
+                                error = "medium";
+                              });
+                            }
+                            // ignore: curly_braces_in_flow_control_structures
+                            else {
+                              setState(() {
+                                error = "good!";
+                              });
+                            }
+                            ;
+                          },
+                          decoration: InputDecoration(
+                            errorText: error,
+                            errorStyle: TextStyle(
+                                color: (error == 'good!')
+                                    ? Colors.green
+                                    : (error == "too short")
+                                        ? Colors.red
+                                        : (error == 'medium')
+                                            ? Colors.amber
+                                            : null),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  VisibleText = !VisibleText;
+                                });
+                              },
+                              icon: Icon((VisibleText)
+                                  ? Icons.visibility
+                                  : (!VisibleText)
+                                      ? Icons.visibility_off_outlined
+                                      : null),
+                            ),
+                            hintText: "password",
+                            filled: true,
+                            fillColor: Colors.deepOrange,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Image.asset('assets/Asset 4.png',
+                          width: MediaQuery.of(context).size.width * 0.55),
+                    ],
+                  ),
+                ),
+                Expanded(
+                    child: GestureDetector(
+                  onDoubleTap: () {},
+                  child: Container(
+                    //height: MediaQuery.of(context).size.height * 0.9,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        borderRadius: BorderRadius.circular(20)),
+                    // child: const Image(
+                    //   image: AssetImage(
+                    //     'assets/Intersect.png',
+                    //   ),
+                    //   fit: BoxFit.cover,
+                    // )
+                    // AssetImage('assets/intersect.png'),
+                  ),
+                ))
+              ]),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
